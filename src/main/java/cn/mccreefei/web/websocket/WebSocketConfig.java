00a9960e8ecad2388ad44a2834e0e1d4911247ca
yangxueyuan <1qaz!QAZ>
2019-12-22 23:26:46 +0800

    加了两个空格

--- src/main/java/cn/mccreefei/web/websocket/WebSocketConfig.java
+++ src/main/java/cn/mccreefei/web/websocket/WebSocketConfig.java
@@ -20,6 +20,8 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
      * 定义接收/websocket时采用wensocket连接，添加HttpSessionHandshakeInterceptor是为了websocket握手前将httpsession中的属性
      * 添加到websocket session中，withSockJS添加对sockJS的支持
      *
+     *
+     *
      * @param stompEndpointRegistry
      */
     public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
